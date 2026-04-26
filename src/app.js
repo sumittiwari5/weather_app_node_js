393c80b88cb3d666ebbe2d517bde7a8ccbf2346e
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-04-26 19:39:38 +0530

    Change page title from 'Weather' to 'Winter is Coming.'

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather",
+    title: "Winter is Coming.",
     name: "Sumit Tiwari",
   });
 });
